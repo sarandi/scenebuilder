f90eb3d2ddda639490d9b3dc749aeaa97d6323a9
Sarandos Klikizos <sarandi@gmail.com>
2026-05-11 14:07:11 -0500

    Upgrade to Next.js 16, migrate middleware to proxy

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config, { dev }) {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
